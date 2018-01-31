111a2297f08b3976753bf71dc8137494ae207978
Koval Nazar <nazarko681@gmail.com>
2018-01-31 15:07:38 +0000

    Initial

--- js/index/index.js
+++ js/index/index.js
@@ -1,45 +1,14 @@
-const btn = document.getElementById('btn');
-const inpt = document.getElementById('inpt');
-const result = document.getElementById('result');
-
-
-document.querySelectorAll('nav.tabs button')
+document.querySelectorAll(' buttons .num')
   .forEach( el => el.addEventListener('click', onClick));
+document.getElementsByClassName("solve")[0]
+  .addEvenListener("click", solvePressed);
+  const $result = document.getElementById("result");
 
-  let value = 0;
+function onClick(ev) {
+   const num = ev.target.innerText;
+   $result.innerText += num;
+}
 
-function onClick() {
-      if (button === 1) {
-        value = 7;
-      } if (button === 2) {
-        value = 8;
-      } if (button === 3) {
-        value = 9;
-      } if (button === 4) {
-        value = "+";
-      } if (button === 5) {
-        value = 4;
-      } if (button === 6) {
-        value = 5;
-      } if (button === 7) {
-        value = 6;
-      } if (button === 8) {
-        value = "-";
-      } if (button === 9) {
-        value = 1;
-      } if (button === 10) {
-        value = 2;
-      } if (button === 11) {
-        value = 3;
-      } if (button === 12) {
-        value = "*";
-      } if (button === 13) {
-        value = " ";
-      } if (button === 14) {
-        value = 0;
-      } if (button === 15) {
-        value = eval(value);
-      } if (button === 16) {
-        value = "/";
-      }
+function solvePressed(ev) {
+  $result.innerText = eval ($result.innerText);
 }
